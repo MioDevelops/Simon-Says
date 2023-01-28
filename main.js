75d4f5f60284b795c52270d5714f3760aa9b6832
MioDevelops <94136291+MioDevelops@users.noreply.github.com>
2023-01-28 11:27:38 -0500

    Almost Done

--- main.js
+++ main.js
@@ -13,5 +13,45 @@ function change_page(page_to) {
 }
 
 function start() {
-    change_page("game");
+    var text = document.getElementById("game_text");
+    var colors = [];
+
+    text.innerText = "";
+
+    var number = Math.floor(Math.random() * 100);
+    if(number <= 25) {
+        colors.push("green");
+    } else if(number <= 50 && number > 25) {
+        colors.push("red");
+    } else if(number <= 75 && number > 50) {
+        colors.push("orange");
+    } else {
+        colors.push("blue");
+    }
+
+    for(let i = 0; i < colors.length; i++) {
+        const colorDiv = document.getElementById(colors[i]);
+        const bg = colorDiv.style.backgroundColor;
+            
+        // Change the color to the lighter color //
+        switch (colors[i]) {
+            case "green":
+                    colorDiv.style.backgroundColor = "green";
+                    break;
+            case "red":
+                    colorDiv.style.backgroundColor = "red";
+                    break;
+            case "orange":
+                colorDiv.style.backgroundColor = "yellow";
+                break;
+            case "blue":
+                colorDiv.style.backgroundColor = "#659BDF";
+                break;
+        }
+
+        // Change the color back to the original color //
+        setTimeout(function() {
+            document.getElementById(colors[i]).style.backgroundColor = bg;
+        }, 1250);
+    }
 }
